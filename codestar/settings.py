1f4070606a89e18bde039c8fbc4ef2a747b37265
Dymtro Chesnokov <dimmando@ukr.net>
2024-09-06 14:30:34 +0000

    Logging text moved to up

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dimmando-djangoblog-ybhubl1npb8.ws.codeinstitute-ide.net', '.herokuapp.com']
 
